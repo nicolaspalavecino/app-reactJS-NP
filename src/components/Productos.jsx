a383ee998e6253b738ddc0881a36c8f64ee75201
Nicolás Palavecino <nmpalav@outlook.com>
2022-12-28 00:59:55 -0300

    Adición de ingredientes y siides

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -2,6 +2,8 @@ import '../styles/Productos.css';
 import ProductCard from './ProductCard.jsx'
 import Prod1Banner from '../img/Prod1-logo.png'
 import Prod2Banner from '../img/Prod2-logo.png'
+import SideIzq from '../img/side-productos-izq.png'
+import SideDer from '../img/side-productos-der.png'
 
 const Productos = () => {
     return(
@@ -14,7 +16,15 @@ const Productos = () => {
                 </div>
                 <img className='banner-titulo' src={Prod2Banner} />
             </div>
-            <ProductCard />
+            <div className='sup-prod-content'>
+                <div className='side-productos side-izq'>
+                    {/* <img src={SideIzq} /> */}
+                </div>
+                <ProductCard />
+                <div className='side-productos side-der'>
+                    {/* <img src={SideDer} /> */}
+                </div>
+            </div>
         </section>
     )
 }
